4569b6c8890a60e74ee8e36262ac5cac41453c6b
Roman Simecek <roman.simecek@cyt.ch>
2011-04-16 09:52:58 +0200

    Initial rails 3 commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delilahs::Application.config.secret_token = 'efc368d5f57ae7b26f5cb45f2d7471f408001977e3eb748089c929f2d60892b2be7a0905ec4eacc2fddd6f8f7bbec09ecb6dd50dab57ba64c395f0a4b85c6989'
